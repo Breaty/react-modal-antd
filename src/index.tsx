da746909fc2f20f1cfa82e3613700cb464ec8feb
白丹 <dan@baidandeMacBook-Pro.local>
2022-06-08 15:53:49 +0800

    修改入口文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import './index.css';
 import AModal from "./component/modal"
 
-const root = createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <AModal />
-  </React.StrictMode>
-);
+export default AModal
+
+// const root = createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <AModal />
+//   </React.StrictMode>
+// );
